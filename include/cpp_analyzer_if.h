5466bc36facdb8812f8d4b2782808a66a79a8d91
Julien Thevenon <julien_thevenon@yahoo.fr>
2012-10-31 19:46:45 +0100

    Update base class to support dedicated diff init method

--- include/cpp_analyzer_if.h
+++ include/cpp_analyzer_if.h
@@ -1,14 +1,14 @@
 #ifndef _CPP_ANALYZER_IF_H_
 #define _CPP_ANALYZER_IF_H_
 
-#include "general_analyzer_if.h"
+#include "diff_analyzer_if.h"
 #include "osm_change.h"
 #include <string>
 #include <vector>
 
 namespace osm_diff_analyzer_cpp_if
 {
-  class cpp_analyzer_if: public osm_diff_analyzer_if::general_analyzer_if
+  class cpp_analyzer_if: public osm_diff_analyzer_if::diff_analyzer_if
   {
   public:
     virtual void analyze(const std::vector<osm_api_data_types::osm_change*> & p_changes)=0;
